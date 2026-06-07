243a0323cf8de14ce7f05198f4a3cb1f9d64c3ad
bbs220 <bsawant357@gmail.com>
2026-06-07 18:40:31 +0530

    almost done with signup

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -33,14 +33,40 @@ export const signup = async (req, res) => {
 
     const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
 
-    const newUser = new User.create({
+    const newUser = await User.create({
       fullName,
       email,
       password,
       profileAvatar: randomAvatar,
     });
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "7d" },
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+      httpOnly: true, // stops XSS attacks by preventing client-side access to the cookie
+      sameSite: "strict", // stops CSRF attacks by only sending the cookie for same-site requests
+      secure: process.env.NODE_ENV === "production", // ensures the cookie is only sent over HTTPS in production
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      // remove this part later
+      user: {
+        id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profileAvatar: newUser.profileAvatar,
+      },
+    });
   } catch (error) {
     console.error(`😭 Error in user signin: ${error}`);
+
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -48,6 +74,8 @@ export const login = async (req, res) => {
   try {
   } catch (error) {
     console.error(`😭 Error in user login: ${error}`);
+
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -55,5 +83,7 @@ export const logout = (req, res) => {
   try {
   } catch (error) {
     console.error(`😭 Error in user logout: ${error}`);
+
+    res.status(500).json({ message: "Internal server error" });
   }
 };
