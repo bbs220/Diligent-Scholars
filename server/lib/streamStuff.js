1392adc942fc7ff839f23a98d39eec35b2c7e482
bbs220 <bsawant357@gmail.com>
2026-06-08 17:14:32 +0530

    stream userdata creation

---
+++ server/lib/streamStuff.js
@@ -0,0 +1,27 @@
+import { StreamChat } from "stream-chat";
+import "dotenv/config";
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error(
+    `Stream API key and secret must be set in environment variables`,
+  );
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userdata) => {
+  try {
+    await streamClient.upsertUsers([userdata]);
+
+    console.log(`🤗 Successfully upserted Stream user: ${userdata.id}`);
+
+    return userdata;
+  } catch (error) {
+    console.error(`😭 Error upserting Stream user: ${error}`);
+  }
+};
+
+export const generateStreamToken = (userId) => {};
