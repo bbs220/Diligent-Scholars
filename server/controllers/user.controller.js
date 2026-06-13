deed78827407e7caa4b3df826136f17ac0c4e432
bbs220 <bsawant357@gmail.com>
2026-06-13 14:34:41 +0530

    almost done user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,35 @@
-export const populateRecommendedUsers = async (req, res) => {};
+import User from "../models/user.model.js";
 
-export const populateFriends = async (req, res) => {};
+export const populateRecommendedUsers = async (req, res) => {
+  try {
+    const currentUserId = req.user.id;
+
+    const currentUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        // exclude the current user
+        { _id: { $ne: currentUserId } },
+        // exclude users who are already friends with the current user
+        { $id: { $nin: currentUser.friends } },
+        // only include users who are onboarded
+        { isOnboarded: true },
+      ],
+    });
+
+    res.status(200).json({ message: "Recommended Users", recommendedUsers });
+  } catch (error) {
+    console.error(`😭 Error populating recommended users: ${error}`);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const populateFriends = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(`😭 Error populating current user's friends: ${error}`);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
