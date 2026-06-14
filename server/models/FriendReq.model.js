fe8ff826ba1f7347dc1bd236b4aa933e64a820e7
bbs220 <bsawant357@gmail.com>
2026-06-14 15:13:11 +0530

    friend req model

---
+++ server/models/FriendReq.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const friendReqSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reqStatus: {
+      type: String,
+      enum: ["pending", "accepted"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
+
+const FriendReq = mongoose.model("FriendRequest", friendReqSchema);
+
+export default FriendReq;
