5b4371836f9f046de0904a8204b63509950ff3aa
bbs220 <bsawant357@gmail.com>
2026-06-18 15:17:01 +0530

    setup toaster

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LogInPage from "./pages/LogInPage";
@@ -19,6 +20,7 @@ function App() {
         <Route path="/call" element={<CallPage />} />
         <Route path="/chat" element={<ChatPage />} />
       </Routes>
+      <Toaster position="bottom-right" />
     </>
   );
 }
