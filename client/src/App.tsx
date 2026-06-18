20ce0df33b316c3cdade67abab66f192d035254e
bbs220 <bsawant357@gmail.com>
2026-06-18 15:12:21 +0530

    setup routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LogInPage from "./pages/LogInPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
+
 function App() {
   return (
-    <div
-      className="w-screen h-screen flex items-center justify-center"
-      data-theme="dark"
-    >
-      <button className="btn btn-accent">hi mom</button>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/onboarding" element={<OnBoardingPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </>
   );
 }
 
