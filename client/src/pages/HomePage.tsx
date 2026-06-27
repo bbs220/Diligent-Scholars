1d9ade43836f031e52436b6fc43bda8b467ff80f
bbs220 <bsawant357@gmail.com>
2026-06-27 22:13:53 +0530

    ui needs to be done on home

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   populateOutgoingFriendReqsFn,
   populateRecommendedUsersFn,
@@ -10,7 +10,7 @@ import {
 const HomePage = () => {
   const queryClient = useQueryClient();
 
-  const [outgoingReqIDs, setOutgoingReqIDs] = useState([]);
+  const [outgoingReqIDs, setOutgoingReqIDs] = useState(new Set());
 
   const { data: friendsData = [], isLoading: loadingUserFriends } = useQuery({
     queryKey: ["friends"],
@@ -37,7 +37,22 @@ const HomePage = () => {
       queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] }),
   });
 
-  return <div>HomePage</div>;
+  useEffect(() => {
+    const outgoingIds = new Set();
+
+    if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
+      outgoingFriendReqs.forEach((req) => {
+        outgoingIds.add(req.id);
+      });
+      setOutgoingReqIDs(outgoingIds);
+    }
+  }, [outgoingFriendReqs]);
+
+  return (
+    <div className="p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto space-y-10"></div>
+    </div>
+  );
 };
 
 export default HomePage;
