ee351ada55a3315714811cafaf734588f40b50c8
bbs220 <bsawant357@gmail.com>
2026-07-01 21:18:54 +0530

    logic done on chat

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
 import { useQuery } from "@tanstack/react-query";
 import { getStreamToken } from "../libs/apiCalls";
+import { StreamChat } from "stream-chat";
+import toast from "react-hot-toast";
+import ChatLoader from "../components/ChatLoader";
+
+const STREAM_API_KEY = import.meta.env.STREAM_API_KEY;
 
 const ChatPage = () => {
   const { id: targetUserId } = useParams();
@@ -13,11 +18,54 @@ const ChatPage = () => {
 
   const { authUser } = useAuthUser();
 
-  const { data } = useQuery({
+  const { data: tokenData } = useQuery({
     queryKey: ["streamToken"],
     queryFn: getStreamToken,
     enabled: !!authUser,
   });
+
+  useEffect(() => {
+    const startChat = async () => {
+      if (!tokenData?.token || !authUser) {
+        return;
+      }
+      try {
+        console.log(`Starting chat with token...`);
+        const client = StreamChat.getInstance(STREAM_API_KEY);
+
+        await client.connectUser(
+          {
+            id: authUser._id,
+            name: authUser.fullName,
+            image: authUser.profileAvatar,
+          },
+          tokenData.token,
+        );
+
+        const channelId = [authUser._id, targetUserId].sort().join("-");
+
+        const currentChannel = client.channel("messaging", channelId, {
+          members: [authUser._id, targetUserId],
+        });
+
+        await currentChannel.watch();
+
+        setChatClient(client);
+        setChatChannel(currentChannel);
+      } catch (error) {
+        console.log(`Error starting chat: ${error}`);
+        toast.error("Failed to start chat. Please try again later");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    startChat();
+  }, []);
+
+  if (loading || !chatClient || !chatChannel) {
+    return <ChatLoader />;
+  }
   return <div>ChatPage</div>;
 };
 
