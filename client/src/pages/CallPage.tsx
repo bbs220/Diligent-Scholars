b0adf395a9ffe852d78a95ac9230cebb57ea69b3
bbs220 <bsawant357@gmail.com>
2026-07-02 15:54:56 +0530

    half done call page

--- client/src/pages/CallPage.tsx
+++ client/src/pages/CallPage.tsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import useAuthUser from "../hooks/useAuthUser";
+import { useQuery } from "@tanstack/react-query";
+import { getStreamToken } from "../libs/apiCalls";
+
 const CallPage = () => {
+  const { id: callId } = useParams<{ id: string }>();
+
+  const [callClient, setCallClient] = useState(null);
+  const [call, setCall] = useState(null);
+  const [connecting, setConnecting] = useState<boolean>(true);
+
+  const { authUser, isLoading } = useAuthUser();
+
+  const { data: tokenData } = useQuery({
+    queryKey: ["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!authUser,
+  });
+
+  useEffect(() => {
+    const startCall = async () => {};
+  }, []);
+
   return <div>CallPage</div>;
 };
 
