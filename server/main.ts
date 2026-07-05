f4442e24b4356aa02b9a07ef85c1710c43acb32b
bbs220 <bsawant357@gmail.com>
2026-07-06 00:33:15 +0530

    graceful shutdown

--- server/main.ts
+++ server/main.ts
@@ -1,15 +1,16 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
 import path from "path";
 import { fileURLToPath } from "url";
 import { connectDB } from "./lib/connectDB.js";
 import { envValidated } from "./lib/envValidated.js";
 
+import { logger } from "./lib/logger.js";
 import authRoutes from "./routes/auth.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import { logger } from "./lib/logger.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -64,7 +65,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   if (envValidated.NODE_ENV === "production") {
     logger.info(`🚀 Server and Client started successfully`);
   } else {
@@ -72,3 +73,36 @@ app.listen(PORT, () => {
   }
   connectDB();
 });
+
+const gracefulShutdown = async (signal: string) => {
+  logger.info(`\n${signal} signal received: closing HTTP server...`);
+
+  server.close(async () => {
+    logger.info("HTTP server closed. Finishing active requests...");
+
+    try {
+      await mongoose.connection.close(false);
+      logger.info("MongoDB connection closed.");
+
+      logger.info("Graceful shutdown complete. Exiting process.");
+      process.exit(0);
+    } catch (error) {
+      logger.error(`Error during MongoDB disconnect: ${error}`);
+      process.exit(1);
+    }
+  });
+
+  // if active requests take longer than 10 seconds, force shutdown
+  setTimeout(() => {
+    logger.error(
+      "Could not close connections in time, forcefully shutting down",
+    );
+    process.exit(1);
+  }, 10000);
+};
+
+// listen for the universal OS signals
+// sent by deployment platforms
+process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+// sent by Ctrl+C in local terminal
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
